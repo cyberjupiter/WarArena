35996c8119ed6982e841555314ae9a223fed1b6c
Aiman Mazlan <aimanlpy@gmail.com>
2022-01-19 08:15:23 +0800

    Main: create character function

--- main.c
+++ main.c
@@ -33,32 +33,48 @@ struct character{
     int type;
 };
 
+void create_character(struct character *character, char *name, int gender, int race, int strength, int dexterity, int luck);
+
 int calculate_stats(struct character *character);
 
 int main(void)
 {
 
     struct character hero_0;
+    struct character hero_1;
+    struct character hero_2;
     
-    //create test character
-    strcpy(hero_0.demographic.name, "Aiman Mazlan");
-    hero_0.demographic.gender = 1;
-    hero_0.demographic.race = 1;
-    hero_0.attr.str = 10;
-    hero_0.attr.dex = 15;
-    hero_0.attr.luck = 0;
+    //create test charactera
+    create_character(&hero_2, "BooBear", 0, 1, 4, 4, 1);
+
 
     calculate_stats(&hero_0);
+    calculate_stats(&hero_1);
+    calculate_stats(&hero_2);
 
-    printf("Attack = %.1f\n", hero_0.attr.stats.attack);
-    printf("Defend = %.1f\n", hero_0.attr.stats.defend);
-    printf("Attack Speed = %.1f\n", hero_0.attr.stats.attack_speed);
-    printf("Critical = %.1f\n", hero_0.attr.stats.critical);
-    printf("HP = %.1f\n", hero_0.attr.stats.hp);
+    // printf("Name = %s\n", hero_0.demographic.name);
+    // printf("Attack = %.1f\n", hero_0.attr.stats.attack);
+    // printf("Defend = %.1f\n", hero_0.attr.stats.defend);
+    // printf("Attack Speed = %.1f\n", hero_0.attr.stats.attack_speed);
+    // printf("Critical = %.1f\n", hero_0.attr.stats.critical);
+    // printf("HP = %.1f\n", hero_0.attr.stats.hp);
+    // printf("\n");
+    // printf("Name = %s\n", hero_1.demographic.name);
+    // printf("Attack = %.1f\n", hero_0.attr.stats.attack);
+    // printf("Defend = %.1f\n", hero_0.attr.stats.defend);
+    // printf("Attack Speed = %.1f\n", hero_0.attr.stats.attack_speed);
+    // printf("Critical = %.1f\n", hero_0.attr.stats.critical);
+    // printf("HP = %.1f\n", hero_0.attr.stats.hp);
+    printf("\n");
+    printf("Name = %s\n", hero_2.demographic.name);
+    printf("Attack = %.1f\n", hero_2.attr.stats.attack);
+    printf("Defend = %.1f\n", hero_2.attr.stats.defend);
+    printf("Attack Speed = %.1f\n", hero_2.attr.stats.attack_speed);
+    printf("Critical = %.1f\n", hero_2.attr.stats.critical);
+    printf("HP = %.1f\n", hero_2.attr.stats.hp);
 
     return 0;
 }
-
 
 int calculate_stats(struct character *character)
 {
@@ -69,3 +85,14 @@ int calculate_stats(struct character *character)
     character->attr.stats.hp = character->attr.str * 10;
     //
 }
+
+void create_character(struct character *character, char *name, int gender, int race, int strength, int dexterity, int luck)
+{
+    strcpy(character->demographic.name, name);
+    character->demographic.gender = gender;
+    character->demographic.race = race;
+    character->attr.str = strength;
+    character->attr.dex = dexterity;
+    character->attr.luck = luck;
+}
+
